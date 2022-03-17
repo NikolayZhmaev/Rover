3ad6d6c05249ea48f29e242b1c1570cd608a2deb
Nikolay Zhmaev <nikolay.zhmaev85@gmail.com>
2022-03-18 00:47:21 +0600

    Верстка слайдера изображений

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,18 @@
-// // Import vendor jQuery plugin example
-// import '~/app/libs/mmenu/dist/mmenu.js'
+import { Swiper, Parallax, Mousewheel } from 'swiper';
+Swiper.use([ Parallax, Mousewheel ])
+
 
 document.addEventListener('DOMContentLoaded', () => {
 
-	// Custom JS
+	const swiperIMG = new Swiper('.slider-img', {
+		loop: false, //не используем зацикленную карусель
+		speed: 2400,
+		parallax: true,
+		mousewheel: {
+			invert: false,
+		}
+
+
+	})
 
 })
