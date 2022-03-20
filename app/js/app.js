413d287ee208f89196440507c877256c1f2a04a7
Nikolay Zhmaev <nikolay.zhmaev85@gmail.com>
2022-03-20 19:33:04 +0600

    Смена и анимация цифр

--- app/js/app.js
+++ app/js/app.js
@@ -20,10 +20,19 @@ import {
 
 document.addEventListener('DOMContentLoaded', () => {
 
+	//Swiper
+
 	const swiperIMG = new Swiper('.slider-img', {
 		loop: false, //не используем зацикленную карусель
 		speed: 2400,
 		parallax: true,
+		pagination: {
+			el: '.slider-pagination-count .total',
+			type: 'custom',
+			renderCustom: function (swiper, current, total) {
+				return `0${total}`
+			}
+		}
 	});
 
 	const swiperText = new Swiper('.slider-text', {
@@ -50,9 +59,11 @@ document.addEventListener('DOMContentLoaded', () => {
 	swiperIMG.controller.control = swiperText;
 	swiperText.controller.control = swiperIMG;
 
-	let gear = document.querySelector('.slider-gear');
 
 	// gear animation
+
+	let gear = document.querySelector('.slider-gear');
+
 	swiperText.on('slideNextTransitionStart', function () {
 		gsap.to(gear, 2.8, {
 			rotation: '+=40',
@@ -66,5 +77,37 @@ document.addEventListener('DOMContentLoaded', () => {
 			ease: Power2.easeInOut,
 		});
 	});
+
+	//Slide Change
+
+	let curnum = document.querySelector('.slider-pagination-count .current'),
+		pagcur = document.querySelector('.slider-pagination-current__num')
+
+	swiperText.on('slideChange', function () {
+		let ind = swiperText.realIndex + 1
+		gsap.to(curnum, .2, {
+			force3D: true,
+			y: -10,
+			opacity: 0,
+			ease: Power2.easeOut,
+			onComplete: function () {
+				gsap.to(curnum, .1, {
+					force3D: true,
+					y: 10
+				})
+				curnum.innerHTML = `0${ind}`;
+				pagcur.innerHTML = `0${ind}`;
+
+			}
+
+		})
+		gsap.to(curnum, .2, {
+			force3D: true,
+			y: 0,
+			opacity: 1,
+			ease: Power2.easeOut,
+			delay: .3
+		})
+	})
 
 });
